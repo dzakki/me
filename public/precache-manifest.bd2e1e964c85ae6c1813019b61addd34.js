2a2893cbca171a779fa329403e812c506ef7eafb
dzakki <dzakkiaz7@gmail.com>
2020-12-15 16:01:13 +0800

    inital commit

---
+++ public/precache-manifest.bd2e1e964c85ae6c1813019b61addd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c158ae679b7596c19e9a1834105ed0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a73d3980c6b5814ed035",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "af43a7c93142e85334fa",
+    "url": "/static/css/main.a18a9ff4.chunk.css"
+  },
+  {
+    "revision": "a73d3980c6b5814ed035",
+    "url": "/static/js/2.ee035998.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ee035998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af43a7c93142e85334fa",
+    "url": "/static/js/main.da2d755a.chunk.js"
+  },
+  {
+    "revision": "796acdd1c91af19e9bf0",
+    "url": "/static/js/runtime-main.94801f4c.js"
+  }
+]);
